5f2d9857465768d24c895da9ebeb7bed0f32ba36
U-CEIBA\jose.sanchez <jose.sanchez@JOSE-SANCHEZ.ceiba.local>
2018-06-18 09:52:09 -0500

    mejoras

--- project/src/main/java/co/ceiba/service/EmailService.java
+++ project/src/main/java/co/ceiba/service/EmailService.java
@@ -3,6 +3,6 @@ package co.ceiba.service;
 public class EmailService {
 
 	 public String sendMail(String message) {
-		 return "Email send succeful with message";
+		 return "Email send succeful with message" + message;
 	 }
 }
